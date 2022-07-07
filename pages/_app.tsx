f4a2545782ed0d3a389aff295cd7e82a987c02e5
Lucija Karan <LKaran@daenet.com>
2022-07-07 03:31:20 -0700

    Pt2Complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import {theme} from "../src/chakra/theme"
+import Layout from '../src/components/Layout/Layout';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+  <ChakraProvider theme={theme}>
+    <Layout>
+     <Component {...pageProps} /> 
+    </Layout>
+  </ChakraProvider>
+  </RecoilRoot>
+  );
 }
 
 export default MyApp
